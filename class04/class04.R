c497720de51f736fde59ebe7ac11c0e43667b612
kadeimanu <kadeimanu@ucsd.edu>
2026-02-24 13:17:24 -0800

    Add Class 4

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Kaliyah Adei-Manu"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="blue",lwd=2)
+
+log(10, base = 10)
+()
